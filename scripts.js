2d7275eea735f32ebcc843a13273dbda64a2b716
Douglas Gardner <douglas@chippy.ch>
2013-03-04 20:56:30 +0000

    add graph

--- scripts.js
+++ scripts.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
         $('#opt-1').text(options[0]);
         $('#opt-2').text(options[1]);
         $('.options').fadeIn("slow");
+        updateStats();
     });
     $('.option').click(function(){
         $('.option').css("background-color","#c20");
@@ -25,8 +26,16 @@ $(document).ready(function() {
                     $('.options').fadeTo('slow',1);
                     $('#opt-1').text(data.next[0]);
                     $('#opt-2').text(data.next[1]);
+                    updateStats();
                 }, "json"
             );
         });
+        updateStats();
     });
 });
+
+function updateStats() {
+    $.getJSON('stats.php', function(data) {
+        $('#chart').sparkline(data, {type:'bar', barColor:'#444444'});
+    });
+};
